4e81709068e2b616f6b58b0843cd19421670516c
Alexander Marchenko <marchenkosasha2@yandex.ru>
2022-04-25 17:06:57 +0300

    Menu fixes

--- handlers/users/menu.py
+++ handlers/users/menu.py
@@ -2,6 +2,7 @@ from aiogram.dispatcher import FSMContext
 from loader import dp
 from aiogram.dispatcher.filters import Command, Text
 from aiogram import types
+from data.temporary_database import one_hour, three_hour, six_hour, twelve_hour, one_day, two_day, arcanas
 
 from keyboards.default import set_data_keyboard, menu_keyboard, delete_data_keyboard
 
@@ -20,8 +21,10 @@ async def get_over_me(message: types.Message, state: FSMContext):
 
 @dp.message_handler(text='Yes', state='delete_data')
 async def get_over_me(message: types.Message, state: FSMContext):
-    await state.set_state('delete_data')
     await state.finish()
+    for item in [one_hour, three_hour, six_hour, twelve_hour, one_day, two_day]:
+        if message.from_user.id in item:
+            item.remove(message.from_user.id)
     await message.answer(text='Your data has been deleted.\nIf you want to use the bot in the future, type /start')
 
 
